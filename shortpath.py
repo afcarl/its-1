6e197d2666cfb6c6f7974a12d603ed839ca85d1b
gnoynait <gnoynait@gmail.com>
2013-04-10 23:25:13 +0800

    delte some unused file

--- shortpath.py
+++
@@ -1,9 +0,0 @@
-""" comput the shortest path between 2 points"""
-
-import numpy as np
-# TODO: fix this method
-def shortpath (grid, origin, dest):
-    pathmap = np.zerors (grid.shape ())
-    queue = []
-    queue.append (dest)
-
