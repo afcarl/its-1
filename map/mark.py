da3fa8c1789da3f986730cee7c15aeb61b799c22
gnoynait <gnoynait@gmail.com>
2013-04-11 17:02:03 +0800

    add clean but not work

--- map/mark.py
+++ map/mark.py
@@ -1,29 +1,28 @@
 import numpy as np
 import sys
+
+cross = sys.maxint
+
 def neighbor (grid, cx, cy):
     n = 0
     if grid[cx, cy] > 0:
         n -= 1
     for x in range (cx - 1, cx + 2):
-        for y range (cy - 1, cy + 2):
+        for y in range (cy - 1, cy + 2):
             if grid[x, y] > 0:
                 n += 1
     return n
 
 def mark (grid, roadmap, x, y, roadid):
-    if grid (x, y) == 0:
+    if grid [x, y] == 0:
         return
     n = neighbor (grid, x, y)
     if n == 0 or roadmap[x, y] != 0:
         return
-    if n == 1:
-        roadmap[x, y] = roadid
-        return
     if n >= 3:
         roadmap[x, y] = sys.maxint
         return
-    if n == 2:
-        roadmap[x, y] = roadid
+    roadmap[x, y] = roadid
     mark (grid, roadmap, x, y + 1, roadid)
     mark (grid, roadmap, x + 1, y - 1, roadid)
     mark (grid, roadmap, x + 1, y, roadid)
@@ -39,4 +38,11 @@ def markroad (grid):
             mark (grid, roadmap, x, y, roadid)
             roadid += 1
         it.iternext ()
+    print roadid
+    return roadmap
+
+if __name__ == '__main__':
+    grid = np.genfromtxt (sys.stdin, dtype = np.int32)
+    roadmap = markroad (grid)
+    np.savetxt (sys.stdout, roadmap, fmt = '%d')
 
