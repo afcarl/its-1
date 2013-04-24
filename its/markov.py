d98b05e8c5239541a6fba9c027be46b3315badae
gnoynait <gnoynait@gmail.com>
2013-04-24 23:05:41 +0800

    fix markov.py

--- its/markov.py
+++ its/markov.py
@@ -6,7 +6,7 @@ def static_markov ():
     reader = csv.reader (sys.stdin)
     markov = {}
     for row in reader:
-        for i in range (8, len (row) - 1):
+        for i in range (8, len (row) - 1, 2):
             if not row[i] in markov:
                 markov[row[i]] = Counter ()
             markov[row[i]][row[i + 2]] += 1
@@ -14,11 +14,11 @@ def static_markov ():
 
 if __name__ == '__main__':
     markov = static_markov ()
+    writer = csv.writer (sys.stdout)
     for passway, cnt in markov.items():
         data = [passway]
-        for p, c in cnt:
+        for p, c in cnt.items():
             data.append (p)
             data.append (c)
-    writer = csv.writer (sys.stdout)
-    writer.writerow (data)
+        writer.writerow (data)
 
