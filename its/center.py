ca44249dd72c17112d42c0ac981c231954ce1c2f
gnoynait <gnoynait@gmail.com>
2013-05-15 16:53:23 +0800

    direction and center

---
+++ its/center.py
@@ -0,0 +1,21 @@
+import numpy as np
+import sys
+import csv
+center = {}
+entity = np.genfromtxt ('data/entitymap.datx', dtype=np.int32)
+width, height = entity.shape
+for i in range (width):
+    for j in range (height):
+        e = entity[i, j]
+        if e >= 10000 or e == 0:
+            continue
+        if e not in center:
+            center[e] = [0, 0, 0]
+        center[e][0] += 1 # count
+        center[e][1] += i # sum x
+        center[e][2] += j # sum y
+
+writer = csv.writer (sys.stdout)
+for c, v in center.items ():
+    writer.writerow ([c, v[1] / v[0], v[2]/v[0]])
+    
--- its/direction.py
+++ its/direction.py
@@ -1,47 +1,44 @@
 import math
+import csv
+center = {}
+centerfile = open ('data/center.datx')
+reader = csv.reader (centerfile)
+for row in reader:
+    center[int (row[0])] = (int (row[1]), int (row[2]))
 
 def direction (origin, dest):
     dx = float(dest[0] - origin[0])
     dy = float(dest[1] - origin[1])
     if dx == 0 and dy >= 0:
-        return 1
+        return 0
     if dx == 0 and dy < 0:
-        return 5
+        return 4
     rate = dy / dx
     if rate > math.tan (math.pi * 3 / 8):
         if dx > 0:
+            return 0
+        return 4
+    elif rate > math.tan (math.pi / 8):
+        if dx > 0:
             return 1
         return 5
-    elif rate > math.tan (math.pi / 8):
+    elif rate > - math.tan (math.pi / 8):
         if dx > 0:
             return 2
         return 6
-    elif rate > - math.tan (math.pi / 8):
+    elif rate > - math.tan (math.pi * 3 / 8):
         if dx > 0:
             return 3
         return 7
-    elif rate > - math.tan (math.pi * 3 / 8):
-        if dx > 0:
-            return 4
-        return 8
     else:
         if dx > 0:
-            return 5
-        return 1
-def oddirection (start, end, entity):
-    width, height = entity.shape
-    for x in range (width):
-        for y in range (height):
-            if entity[x, y] == start:
-                s = (x, y)
-            if entity[x, y] == end:
-                e = (x, y)
-    return direction (s, e)
+            return 4
+        return 0
+def oddirection (start, end):
+    return direction (center[start], center[end])
 if __name__ == '__main__':
-    import numpy as np
     import sys
     start = int(sys.argv[1])
     end = int(sys.argv[2])
-    entity = np.genfromtxt ('data/entitymap.datx', dtype = np.int32)
-    print (oddirection (start, end, entity))
+    print (oddirection (start, end))
 
